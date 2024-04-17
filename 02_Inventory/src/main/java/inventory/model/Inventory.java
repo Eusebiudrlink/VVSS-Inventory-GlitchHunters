48b1970fe040eba65e414db0b940004ed7a6d5e6
Eusebiudrlink <sebiant@yahoo.com>
2024-04-17 20:35:08 +0300

    lab3

--- 02_Inventory/src/main/java/inventory/model/Inventory.java
+++ 02_Inventory/src/main/java/inventory/model/Inventory.java
@@ -5,136 +5,164 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class Inventory {
-    
+
     // Declare fields
     private ObservableList<Product> products;
     private ObservableList<Part> parts;
     private int autoPartId;
     private int autoProductId;
-    public Inventory(){
+
+    public Inventory() {
         this.products = FXCollections.observableArrayList();
         this.parts = FXCollections.observableArrayList();
-        this.autoProductId=0;
-        this.autoPartId=0;
+        this.autoProductId = 0;
+        this.autoPartId = 0;
     }
 
     // Declare methods
+
     /**
      * Add new product to observable list products
-     * @param product 
+     *
+     * @param product
      */
     public void addProduct(Product product) {
         products.add(product);
     }
-    
+
     /**
      * Remove product from observable list products
-     * @param product 
+     *
+     * @param product
      */
     public void removeProduct(Product product) {
         products.remove(product);
     }
-    
+
     /**
      * Accepts search parameter and if an ID or name matches input, that product is returned
+     *
      * @param searchItem
-     * @return 
+     * @return
      */
     public Product lookupProduct(String searchItem) {
-        for(Product p: products) {
-            if(p.getName().contains(searchItem) || (p.getProductId()+"").equals(searchItem))return p;
+        for (Product p : products) {
+            if (p.getName().contains(searchItem) || (p.getProductId() + "").equals(searchItem)) return p;
         }
         return null;
     }
-    
+
     /**
      * Update product at given index
+     *
      * @param index
-     * @param product 
+     * @param product
      */
     public void updateProduct(int index, Product product) {
         products.set(index, product);
     }
-    
+
     /**
      * Getter for Product Observable List
-     * @return 
+     *
+     * @return
      */
     public ObservableList<Product> getProducts() {
         return products;
     }
 
     public void setProducts(ObservableList<Product> list) {
-        products=list;
+        products = list;
     }
-    
+
     /**
      * Add new part to observable list allParts
-     * @param part 
+     *
+     * @param part
      */
     public void addPart(Part part) {
         parts.add(part);
     }
-    
+
     /**
      * Removes part passed as parameter from allParts
-     * @param part 
+     *
+     * @param part
      */
     public void deletePart(Part part) {
         parts.remove(part);
     }
-    
+
     /**
      * Accepts search parameter and if an ID or name matches input, that part is returned
+     *
      * @param searchItem
-     * @return 
+     * @return
      */
+    //initial code
+//    public Part lookupPart(String searchItem) {
+//        for(Part p: parts) {
+//            if(p.getName().contains(searchItem) || (p.getPartId()+"").equals(searchItem))
+//                return p;
+//        }
+//        return null;
+//    }
     public Part lookupPart(String searchItem) {
-        for(Part p: parts) {
-            if(p.getName().contains(searchItem) || (p.getPartId()+"").equals(searchItem)) return p;
+        1 Part part = null;
+        2 if (searchItem != null) {
+            3 for (Part p : parts) {
+                4 if (p.getName().contains(searchItem) || (p.getPartId() + "").equals(searchItem))
+                    5 part = p
+            }
         }
-        return null;
-    }
-    
+        6 if (part != null) {
+            7 return part
+        }
+        8 return null;
+ 10 }
+
     /**
      * Update part at given index
+     *
      * @param index
-     * @param part 
+     * @param part
      */
     public void updatePart(int index, Part part) {
         parts.set(index, part);
     }
-    
+
     /**
      * Getter for allParts Observable List
-     * @return 
+     *
+     * @return
      */
     public ObservableList<Part> getParts() {
         return parts;
     }
 
     /**
-     *
      * @param list
      */
     public void setParts(ObservableList<Part> list) {
-        parts =list;
+        parts = list;
     }
-    
+
     /**
      * Method for incrementing part ID to be used to automatically
      * assign ID numbers to parts
-     * @return 
+     *
+     * @return
      */
     public int getAutoPartId() {
         autoPartId++;
         return autoPartId;
     }
-    
+
     /**
      * Method for incrementing product ID to be used to automatically
      * assign ID numbers to products
-     * @return 
+     *
+     * @return
      */
     public int getAutoProductId() {
         autoProductId++;
@@ -142,12 +170,12 @@ public class Inventory {
     }
 
 
-    public void setAutoPartId(int id){
-        autoPartId=id;
+    public void setAutoPartId(int id) {
+        autoPartId = id;
     }
 
-    public void setAutoProductId(int id){
-        autoProductId=id;
+    public void setAutoProductId(int id) {
+        autoProductId = id;
     }
-    
+
 }
